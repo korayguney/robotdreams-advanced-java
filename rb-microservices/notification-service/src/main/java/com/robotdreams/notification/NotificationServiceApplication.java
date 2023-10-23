511fecfc6c5fa131791e11255542b50dacae62cd
Koray Güney <koray.guney@hotmail.com>
2023-10-23 22:06:16 +0400

    added api gateway and rabbitmq

--- rb-microservices/notification-service/src/main/java/com/robotdreams/notification/NotificationServiceApplication.java
+++ rb-microservices/notification-service/src/main/java/com/robotdreams/notification/NotificationServiceApplication.java
@@ -1,15 +1,43 @@
 package com.robotdreams.notification;
 
+import com.robotdreams.notification.config.NotificationConfig;
+import com.robotdreams.notification.model.Notification;
+import com.robotdreams.rabbitmq.RabbitMQMessageProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+import java.time.LocalDateTime;
+
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.robotdreams.notification",
+                "com.robotdreams.rabbitmq"
+        }
+)
 @EnableEurekaClient
-public class NotificationServiceApplication {
+public class NotificationServiceApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
     }
 
+    @Autowired
+    RabbitMQMessageProducer producer;
+
+    @Autowired
+    NotificationConfig config;
+
+    @Override
+    public void run(String... args) throws Exception {
+        // producer.publish("Test Test...", config.getNotificationExchange(), config.getNotificationRoutingKey());
+        producer.publish(Notification.builder()
+                        .toCustomerPhone("1111111")
+                        .sender("aaaa")
+                        .build(),
+                config.getNotificationExchange(),
+                config.getNotificationRoutingKey());
+    }
 }
