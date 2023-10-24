a38309a7e85297c3201818317cfb202c2d5578aa
Koray Güney <koray.guney@hotmail.com>
2023-10-24 20:31:23 +0400

    added api gateway and rabbitmq

---
+++ rb-dockerfile/src/main/java/com/robotdreams/rbdockerfile/RbDockerfileApplication.java
@@ -0,0 +1,13 @@
+package com.robotdreams.rbdockerfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbDockerfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RbDockerfileApplication.class, args);
+    }
+
+}
---
+++ rb-dockerfile/src/test/java/com/robotdreams/rbdockerfile/RbDockerfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robotdreams.rbdockerfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbDockerfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
